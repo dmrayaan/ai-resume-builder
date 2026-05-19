5d3bac7a1d7a354a55f435e75944416e33790d0c
AyaanDMR <ayaan786tube@gmail.com>
2026-05-19 17:54:44 +0530

    Fix Next dependencies

--- GenieResumay/src/lib/groq.ts
+++ GenieResumay/src/lib/groq.ts
@@ -1,7 +1,7 @@
 
-import Groq from "groq-sdk";
-const apiKey = process.env.GROQ_API_KEY;
-const groq = new Groq({apiKey,dangerouslyAllowBrowser: true});
+import Groq from "groq-sdk"
+
+const groq = new Groq({apiKey: process.env.OPENROUTER_API_KEY,dangerouslyAllowBrowser: true});
 
 
 export default groq
